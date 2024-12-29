d21839cd373ab9d71cd6a7bdf8c087e36ee12fc5
Malte H <123840987+Hutty36@users.noreply.github.com>
2024-12-30 00:40:44 +0100

    Update loadingbar.py

--- loadingbar.py
+++ loadingbar.py
@@ -5,15 +5,16 @@ print("Hello")
 x = 123
 progress = 0
 prev_progress = 0
-print(" " + "_" * 34 + " . . . work in progress . . . " + "_" * 34 + " ")
+print(" " + "_" * 35 + " . . . work in progress . . . " + "_" * 35 + " ")
+print("\r" + "[" + " " * (100) + "]" + " 0%", end='', flush=True)
 for i in range (x):
-    progress = int((i / x) * 100) - 1
+    progress = int(((i+1) / x) * 100)
     if (progress > prev_progress and progress > 0):
-        print("\r" + "[" + "#" * progress + " " * (98-progress) + "]", end='', flush=True)
+        print("\r" + "[" + "#" * progress + " " * (100-progress) + "]" + f" {progress}%", end='', flush=True)
     prev_progress = progress
 
     #content of the loop represented by time.sleep()
-    time.sleep(0.05)
-print()
+    time.sleep(5)
+print("\n")
 
-print("\nWorld")
+print("World")
